1b196ea6927fd3990648740c391809ce81ccc4c8
lebRonFeng <18292996148@163.com>
2024-05-21 16:16:11 +0800

    计算属性

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,49 @@
 <template>
   <div id="app">
     <h1>App组件</h1>
-    <Avartar :url="require('@/assets/cq.webp')" :size="50"/>
+    <p>姓：{{firstName}}</p>
+    <p>名：{{lastName}}</p>
+    <!-- <p>全名：{{firstName + lastName}}</p> -->
+    <p>全名：{{ getFullName() }}</p>
+    <p>全名：{{ fullName }}</p>
   </div>
 </template>
 
 <script>
-import Avartar from "./components/Avatar"
 export default {
   name: 'App',
-  components: {
-    Avartar
+  data(){
+    return {
+      firstName: "勒布朗",
+      lastName: "詹姆斯"
+    }
+  },
+  computed:{
+    fullName() {
+      return this.firstName + "·" + this.lastName;
+    }
+  },
+  methods: {
+    getFullName() {
+      return this.firstName + "·" + this.lastName;
+    }
   }
+
+
+  // 复杂计算属性
+  // computed:{
+  //   fullName() {
+  //     get(){
+  //       return this.firstName + "·" + this.lastName;
+  //     },
+  //     set(val){
+  //       this.firstName = val[0];
+  //       this.lastName = val.substr(1)
+  //     }
+  //   }
+  // },
 }
+
 </script>
 
 <style></style>
