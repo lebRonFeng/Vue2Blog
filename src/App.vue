8766165429bbfcfc17e57c7b1d5e458c35ed9d0a
lebRonFeng <18292996148@163.com>
2024-05-21 14:06:17 +0800

    vue2博客项目搭建

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>App组件</h1>
+    <Avartar :url="require('@/assets/cq.webp')" :size="50"/>
+  </div>
+</template>
+
+<script>
+import Avartar from "./components/Avatar"
+export default {
+  name: 'App',
+  components: {
+    Avartar
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/Avatar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <img :src="url" class="avatar-img"
+            :style="{width: size + 'px',height: size + 'px',}">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Avatar",
+    props: {
+        url: {
+            type: String,
+            required: true
+        },
+        size: {
+            type: Number,
+        }
+    }
+}
+</script>
+
+<style scoped>
+.avatar-img{
+    border-radius: 50%;
+    object-fit: cover;
+    display: block;
+}
+</style>
