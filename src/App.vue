d194f1cd3cab04d614c8d9e5d63115a0a9ccdee2
lebRonFeng <18292996148@163.com>
2024-05-21 17:02:44 +0800

    提交Icon图标组件

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,18 @@
 <template>
   <div id="app">
     <h1>App组件</h1>
-    <p>姓：{{firstName}}</p>
-    <p>名：{{lastName}}</p>
-    <!-- <p>全名：{{firstName + lastName}}</p> -->
-    <p>全名：{{ getFullName() }}</p>
-    <p>全名：{{ fullName }}</p>
+    <Icon type="home"/>
   </div>
 </template>
 
 <script>
+import Icon from "./components/Icon.vue"
 export default {
   name: 'App',
-  data(){
-    return {
-      firstName: "勒布朗",
-      lastName: "詹姆斯"
-    }
-  },
-  computed:{
-    fullName() {
-      return this.firstName + "·" + this.lastName;
-    }
-  },
-  methods: {
-    getFullName() {
-      return this.firstName + "·" + this.lastName;
-    }
+  components: {
+    Icon
   }
 
-
-  // 复杂计算属性
-  // computed:{
-  //   fullName() {
-  //     get(){
-  //       return this.firstName + "·" + this.lastName;
-  //     },
-  //     set(val){
-  //       this.firstName = val[0];
-  //       this.lastName = val.substr(1)
-  //     }
-  //   }
-  // },
 }
 
 </script>
