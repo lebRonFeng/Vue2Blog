0313e9291db4484b33349deccd38c24ca587215d
lebRonFeng <18292996148@163.com>
2024-05-22 11:36:03 +0800

    提交图片加载组件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <div id="app">
     <h1>App组件</h1>
-    <!-- <Icon type="home"/> -->
     <div class="test-container">
-      <Empty/>
+      <ImageLoader 
+        src="https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?fit-crop&crop=entropy&w=3456&h=2304" 
+        placeholder="https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?w=100" 
+        :duration="3000"
+        @load="handleLoaded"/>
     </div>
 
   </div>
 </template>
 
 <script>
-import Empty from "./components/Empty"
-// import Icon from "./components/Icon.vue"
+import ImageLoader from "./components/ImageLoader"
 export default {
   name: 'App',
   components: {
-    Empty
-  }
+    ImageLoader
+  },
+  data() {
+    return {
 
+    }
+  },
+methods:{
+  handleLoaded() {
+    console.log("图片加载完成")
+  }
+}
 }
 
 </script>
@@ -28,6 +39,6 @@ export default {
   height: 400px;
   border: 2px solid;
   margin: 0 auto;
-  position: relative;
+  /* position: relative; */
 }
 </style>
