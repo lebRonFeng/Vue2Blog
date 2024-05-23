a144f675da323672b4e17e90233beeea52677741
lebRonFeng <18292996148@163.com>
2024-05-23 10:15:41 +0800

    使用css module

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "./styles/global.less"
 import router from './router'
 Vue.config.productionTip = false
 
+// 测试一下纯DOM操作
+import styles from "./styles/message.module.less";
+console.log(styles)
+const div = document.createElement("div");
+div.className = styles.message;
+div.innerText = "qwert";
+document.body.appendChild(div)
 
 new Vue({
   router,
