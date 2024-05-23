b25b0549987be8ce0de4d57410e108cbb2e4c1dd
lebRonFeng <18292996148@163.com>
2024-05-23 10:24:00 +0800

    得到组件渲染的Dom

--- src/main.js
+++ src/main.js
@@ -4,13 +4,25 @@ import "./styles/global.less"
 import router from './router'
 Vue.config.productionTip = false
 
-// 测试一下纯DOM操作
-import styles from "./styles/message.module.less";
-console.log(styles)
-const div = document.createElement("div");
-div.className = styles.message;
-div.innerText = "qwert";
-document.body.appendChild(div)
+// 测试一下纯DOM操作 
+// import styles from "./styles/message.module.less";
+// console.log(styles)
+// const div = document.createElement("div");
+// div.className = styles.message;
+// div.innerText = "qwert";
+// document.body.appendChild(div)
+
+// 得到组件生成的根DOM
+function getComponentRootDom(comp, props){
+  const vm = new Vue({
+    render: (h) => h(comp, { props }),
+  });
+  vm.$mount();
+  return vm.$el;
+}
+import Icon from "./components/Icon";
+var dom = getComponentRootDom(Icon, { type: "home"})
+console.log(dom)
 
 new Vue({
   router,
