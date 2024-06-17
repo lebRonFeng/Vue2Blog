f70b20dd300521cb4ad575026dedf710fece10f1
lebRonFeng <18292996148@163.com>
2024-06-17 19:16:54 +0800

    组件递归demo提交

--- src/views/Blog/index.vue
+++ src/views/Blog/index.vue
@@ -1,28 +1,58 @@
 <template>
-  <div style="position:relative">
+  <div>
     <h1>博客</h1>
-    <Loading v-if="isLoading"/>
-    <button @click="isLoading = !isLoading">切换显示/隐藏</button>
+    <RightList :list="list" @select="handleClick" />
   </div>
-  
 </template>
 
 <script>
-import Loading from "@/components/Loading"
+import RightList from "./RightList";
 
 export default {
-  components:{
-    Loading
+  components: {
+    RightList,
   },
-  data(){
+  data() {
     return {
-      isLoading: true,
-      
-    }
-  }
-}
+      list: [
+        {
+          name: "a",
+          isSelect: false,
+        },
+        {
+          name: "b",
+          isSelect: false,
+        },
+        {
+          name: "c",
+          isSelect: true,
+          children: [
+            { name: "c-1", isSelect: false },
+            { name: "c-2", isSelect: false },
+            {
+              name: "c-3",
+              isSelect: false,
+              children: [
+                { name: "c-3-1", isSelect: false },
+                { name: "c-3-2", isSelect: false },
+                { name: "c-3-3", isSelect: false },
+              ],
+            },
+          ],
+        },
+        {
+          name: "d",
+          isSelect: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    handleClick(item) {
+      console.log(item, "item");
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
